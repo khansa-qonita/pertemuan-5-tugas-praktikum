a65ad728135a1bbdd18d86498cd422b1d5e6c611
khansa-qonita <khansaqonita420@gmail.com>
2026-04-01 14:44:58 +0700

    mengimplementasi logika cekPrima menggunakan while loop

--- looping.cpp
+++ looping.cpp
@@ -10,3 +10,13 @@ int pilihan;
 
 bool cekPrima(int n);
 bool cekFibonacci(int n);
+
+bool cekPrima(int n) {
+    if (n <= 1) return false;
+    int i = 2;
+    while (i * i <= n) {
+        if (n % i == 0) return false;
+        i++;
+    }
+    return true;
+}
